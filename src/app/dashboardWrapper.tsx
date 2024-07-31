f703d220d19f8ee4fd90d6ba717e06c4bc4e8d29
Piyush yadav <piyushyadav0191@gmail.com>
2024-07-31 10:02:41 +0530

    stlying for navbar

---
+++ src/app/dashboardWrapper.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Navbar from './(components)/Navbar'
+
+type Props = {
+    children: React.ReactNode
+}
+
+const DashboardWrapper = ({children}: Props) => {
+  return (
+    <div className='light dark:dark flex bg-gray-50 text-gray-900 w-full min-h-screen'>
+      Sidebar
+      <main className={`flex flex-col w-full h-full py-7 bg-gray-200 md:pl-24`}>
+        <Navbar />
+        {children}
+      </main>
+    </div>
+  )
+}
+
+export default DashboardWrapper
