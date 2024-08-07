e37a3d202245568224832200ac0a58143859aeb1
Piyush yadav <piyushyadav0191@gmail.com>
2024-08-07 09:11:41 +0530

    Add: toggle sidebar

--- src/app/(components)/Sidebar/index.tsx
+++ src/app/(components)/Sidebar/index.tsx
@@ -1,11 +1,23 @@
 "use client";
 
+import { useAppDispatch, useAppSelector } from "@/redux/redux";
+import { setIsSidebarCollapsed } from "@/redux/state";
 import { Menu } from "lucide-react";
 import React from "react";
 
 const Sidebar = () => {
+  const dispatch = useAppDispatch();
+  const isSidebarCollapsed = useAppSelector((state) => state.global.isSidebarCollapsed);
+
+  const toggleSidebar = () => {
+    dispatch(setIsSidebarCollapsed(!isSidebarCollapsed));
+  }
+
+  const sidebarClassnames = `fixed flex flex-col ${isSidebarCollapsed ? "w-0 md:w-16": "w-72 md:w-64"} bg-white transition-all duration-300 ease-in-out overflow-hidden shadow-md z-40 h-full border-r border-gray-200`;
+
+
   return (
-    <div>
+    <div className={sidebarClassnames}>
       {/* top logo  */}
       <div className="flex gap-3 justify-between md:justify-normal items-center pt-8">
         <div>Logo</div>
@@ -13,7 +25,7 @@ const Sidebar = () => {
 
         <button
           className="md:hidden px-3 py-2 bg-gray-100 rounded-full hover:bg-blue-100"
-          onClick={() => {}}
+          onClick={toggleSidebar}
         >
           <Menu className="w-4 h-4" />
         </button>
--- src/app/dashboardWrapper.tsx
+++ src/app/dashboardWrapper.tsx
@@ -1,17 +1,31 @@
-import React from "react";
+"use client"
+
+import React, { useEffect } from "react";
 import Navbar from "./(components)/Navbar";
 import Sidebar from "./(components)/Sidebar";
-import StoreProvider from "@/redux/redux";
+import StoreProvider, { useAppSelector } from "@/redux/redux";
 
 type Props = {
   children: React.ReactNode;
 };
 
 const DashboardLayout = ({ children }: Props) => {
+
+  const isSidebarCollapsed = useAppSelector((state) => state.global.isSidebarCollapsed);
+  const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
+
+  useEffect(() => {
+    if(isDarkMode){
+      document.documentElement.classList.add("dark")
+    } else {
+      document.documentElement.classList.add("light")
+    }
+  }, [])
+
   return (
-    <div className="light dark:dark flex bg-gray-50 text-gray-900 w-full min-h-screen">
+    <div className={` ${isDarkMode ? "dark": "light"}  flex bg-gray-50 text-gray-900 w-full min-h-screen`}>
       <Sidebar />
-      <main className={`flex flex-col w-full h-full py-7 bg-gray-200 md:pl-24`}>
+      <main className={`flex flex-col w-full h-full py-7 bg-gray-200 ${isSidebarCollapsed ? "md:pl-24": "md:pl-72"}  `}>
         <Navbar />
         {children}
       </main>
